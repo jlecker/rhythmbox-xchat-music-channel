87c434b0526e322e50774f383c5929766a504e9b
James Lecker <jlecker@gmail.com>
2010-11-12 15:42:12 -0600

    always check for the channel before announcing

--- xchat_music_channel/__init__.py
+++ xchat_music_channel/__init__.py
@@ -12,57 +12,61 @@ DBusGMainLoop(set_as_default=True)
 
 class XChatMusicChannelPlugin(rb.Plugin):
     def activate(self, shell):        
-        self.bus = dbus.SessionBus()
-        self.xchat_object = self.bus.get_object('org.xchat.service', '/org/xchat/Remote')
-        xchat = dbus.Interface(self.xchat_object, 'org.xchat.plugin')
-        
         gc = gconf.client_get_default()
-        server = gc.get_string(gconf_keys['server'])
-        channel = gc.get_string(gconf_keys['channel'])
-        self.xchat_context = xchat.FindContext(server, channel)
+        self.server = gc.get_string(gconf_keys['server'])
+        self.channel = gc.get_string(gconf_keys['channel'])
         
-        if self.xchat_context:
-            self.shell = shell
-            self.player = shell.get_player()
-            self.event_id = self.player.connect('playing-song-changed', self.song_changed)
-            
-            self.xchat_hook = xchat.HookPrint('Channel Message', 0, 0)
-            self.bus.add_signal_receiver(
-                self.got_message,
-                'PrintSignal',
-                'org.xchat.plugin',
-                'org.xchat.service',
-                '/org/xchat/Remote'
-            )
+        self.shell = shell
+        self.player = shell.get_player()
+        self.event_id = self.player.connect('playing-song-changed', self.song_changed)
+        
+        self.bus = dbus.SessionBus()
+        self.signal = None
+        self.xchat_object = None
+        self.xchat_hook = None
+        self.xchat_context = None
     
     def deactivate(self, shell):
-        if self.xchat_context:
-            self.bus.remove_signal_receiver(
-                self.got_message,
-                'PrintSignal',
-                'org.xchat.plugin',
-                'org.xchat.service',
-                '/org/xchat/Remote'
-            )
-            self.player.disconnect(self.event_id)
-            xchat = dbus.Interface(self.xchat_object, 'org.xchat.plugin')
-            xchat.Unhook(self.xchat_hook)
-            del self.event_id
-            del self.xchat_hook
-            del self.player
-            del self.shell
         del self.xchat_context
-        del self.xchat_object
+        if self.xchat_hook:
+            self.signal.remove()
+            self.get_xchat().Unhook(self.xchat_hook)
+        del self.xchat_hook
+        self.player.disconnect(self.event_id)
+        del self.event_id
+        del self.player
+        del self.shell
+        del self.channel
+        del self.server
+        del self.signal
         del self.bus
     
+    def get_xchat(self):
+        xchat_object = self.bus.get_object('org.xchat.service', '/org/xchat/Remote')
+        return dbus.Interface(xchat_object, 'org.xchat.plugin')
+    
     def song_changed(self, player, entry):
-        artist = self.shell.props.db.entry_get(entry, rhythmdb.PROP_ARTIST)
-        title = self.shell.props.db.entry_get(entry, rhythmdb.PROP_TITLE)
-        album = self.shell.props.db.entry_get(entry, rhythmdb.PROP_ALBUM)
-        
-        xchat = dbus.Interface(self.xchat_object, 'org.xchat.plugin')
-        xchat.SetContext(self.xchat_context)
-        xchat.Command('say Playing: %s - %s (%s)' % (artist, title, album))
+        xchat = self.get_xchat()
+        self.xchat_context = xchat.FindContext(self.server, self.channel)
+        if self.xchat_context:
+            artist = self.shell.props.db.entry_get(entry, rhythmdb.PROP_ARTIST)
+            title = self.shell.props.db.entry_get(entry, rhythmdb.PROP_TITLE)
+            album = self.shell.props.db.entry_get(entry, rhythmdb.PROP_ALBUM)
+            xchat.SetContext(self.xchat_context)
+            xchat.Command('say Playing: %s - %s (%s)' % (artist, title, album))
+            if not self.xchat_hook:
+                self.xchat_hook = xchat.HookPrint('Channel Message', 0, 0)
+                self.signal = self.bus.add_signal_receiver(
+                    self.got_message,
+                    'PrintSignal',
+                    'org.xchat.plugin',
+                    'org.xchat.service',
+                    '/org/xchat/Remote'
+                )
+        elif self.xchat_hook:
+            self.signal.remove()
+            xchat.Unhook(self.xchat_hook)
+            self.xchat_hook = None
     
     def got_message(self, data, priority, context):
         if context == self.xchat_context:
